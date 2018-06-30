dfcee96aea918cff59dd30acd734c7e86251b46b
jose pata roque <jose.pataroque@gmail.com>
2018-06-29 20:46:54 -0500

    inicio

---
+++ src/controllers/user/ViewContraller.java
@@ -0,0 +1,50 @@
+package controllers.user;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+import java.util.List;
+
+import javax.jdo.PersistenceManager;
+import javax.jdo.Query;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+import controllers.factura.PMF;
+import models.factura.*;
+
+	@SuppressWarnings("serial")
+	public class ViewContraller extends HttpServlet {
+		public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+			resp.setContentType("text/html");
+			
+			
+			PersistenceManager pm = PMF.get().getPersistenceManager();
+			Query query = pm.newQuery(Factura.class);
+
+			//query.setOrdering("nombre descending");
+			// query for the entities by name
+			
+			
+			List<Factura>array = (List<Factura>)query.execute("select from Factura");
+			 
+			req.setAttribute("array",array);
+			try {
+				try {
+					req.getRequestDispatcher("/WEB-INF/view/view.jsp").forward(req, resp);
+				} catch (ServletException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				query.closeAll();
+			
+			}
+			finally {
+				pm.close();
+			}
+		}
+	}
+
+
---
+++ src/models/user/User.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models.user;
+
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.IdentityType;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
+
+
+@PersistenceCapable(identityType = IdentityType.APPLICATION)
+public class User {
+    @PrimaryKey
+    @Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY) private Long codigo;
+    @Persistent private Date date;
+    @Persistent private String customer;
+    @Persistent private String pension;
+    @Persistent private Double price;
+    @Persistent private boolean status;
+    @Persistent private String address;
+    
+   public User(String customer, String pension,Double price,String address) {
+	this.customer=customer;
+	this.pension=pension;
+	this.price=price;
+	this.address=address;
+	TimeZone tz = TimeZone.getTimeZone ("America / Lima");
+	Calendar calendar=new GregorianCalendar(tz);	
+	this.date=(Date) calendar.getTime();
+	this.status=true;
+	
+	
+}
+    public void setAddress(String address) {
+		this.address = address;
+	}
+    public void setCustomer(String customer) {
+		this.customer = customer;
+	}
+    public void setPrice(Double price) {
+		this.price = price;
+	}
+    public void setPension(String pension) {
+		this.pension = pension;
+	}
+    public void setStatus(boolean status) {
+			this.status=status;
+	}
+   public String getAddress() {
+	return address;
+}
+   public String getCustomer() {
+	return customer;
+}
+   public Double getPrice() {
+	return price;
+}
+   public String getPension() {
+	return pension;
+}
+   public String getStatus(){
+	  String Estado="sin estado";
+	   if (status==true) {
+		    Estado="cancelado";
+		
+		}else if(status==false){
+			    Estado="pendiente";
+	
+		}
+	   return Estado;
+   }
+   
+   public String getDate() {
+	DateFormat dateformat=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss ");  
+	//   DateFormat dateformat=new SimpleDateFormat("dd MMMM 'del' YYYY  HH:mm:s");  
+
+	   String da=dateformat.format(date).toString();
+	return da;
+}
+   public void setDate() {
+	   TimeZone tz = TimeZone.getTimeZone ("America / Lima");
+		Calendar calendar=new GregorianCalendar(tz);	
+		this.date=(Date) calendar.getTime();
+}  
+   public Long getCodigo() {
+	return codigo;
+}
+}
